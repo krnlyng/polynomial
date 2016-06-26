cf83b827ea903581c7c2672936cb5cbdcaecceb9
Frajo <f_haider@gmx.at>
2016-06-26 17:09:05 +0200

    implement taylor polynom generation for a sample polynom while using different expansion points at each interation to test the various polynomial operations

--- main.cpp
+++ main.cpp
@@ -3,16 +3,45 @@
 
 #include "polynomial.h"
 
+using namespace std;
+
 int main(int argc, char *argv[])
 {
-    polynomial<double> a, b;
+    polynomial<double> a;
+    vector<double> expansion_points;
+
     a.set(0, 1.f);
     a.set(2, 1.f);
-    b.set(1, 2.f);
-    b.set(4, 2.f);
+    a.set(5, 0.5f);
 
-    std::cout << a+b << std::endl;
+    cout << a << endl;
 
+    expansion_points.push_back(2.f);
+    expansion_points.push_back(1.f);
+    expansion_points.push_back(-1.f);
+    expansion_points.push_back(25.f);
+    expansion_points.push_back(-25.f);
+    expansion_points.push_back(5.f);
+
+    polynomial<double> d = a;
+    polynomial<double> result;
+    for(vector<double>::size_type i = 0;i < expansion_points.size();i++)
+    {
+        polynomial<double> e = d.eval_at(expansion_points[i]);
+
+        polynomial<double> tmp;
+        tmp.set(0, 1.f);
+        for(vector<double>::size_type j = 0;j < i;j++)
+        {
+            tmp = tmp.indefinite_integral() - tmp.indefinite_integral().eval_at(expansion_points[i-j - 1]);
+        }
+
+        result = result + e * tmp;
+
+        d = d.differentiate();
+    }
+
+    cout << result << endl;
     return 0;
 }
 
